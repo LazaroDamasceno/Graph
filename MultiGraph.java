231b195be99ba497bfc4e7545b8bb3fe5955dd9d
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2023-06-12 17:09:57 +0000

    commit

--- MultiGraph.java
+++ MultiGraph.java
@@ -3,17 +3,6 @@ import java.util.stream.Collectors;
 
 public class MultiGraph {
 	
-	@Override
-	public String toString() {
-		return vertices
-			.keySet()
-			.stream()
-			.collect(
-				Collectors.toMap(
-					key -> key, 
-					value -> vertices.get(value)
-			))
-			.toString();
-	}
+
 	
 }
