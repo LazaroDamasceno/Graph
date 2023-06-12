ebccda9e8f1d51a505194a7bf96b481f46375269
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2023-06-12 17:30:30 +0000

    commit

--- MultiGraph.java
+++ MultiGraph.java
@@ -1,7 +1,24 @@
-import java.util.*;
+import java.util.List;
 
 public class MultiGraph {
 	
-	private Map<
+	private List<Vertex> v;
+	private List<Edge> e;
+	
+	public List<Vertex> getV() {
+		return v;
+	}
+
+	public void setV(List<Vertex> v) {
+		this.v = v;
+	}
+
+	public List<Edge> getE() {
+		return e;
+	}
+
+	public void setE(List<Edge> e) {
+		this.e = e;
+	}
 	
 }
