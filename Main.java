5cbcd13836a105dda2ba9ae4e8494bf5644d1cc3
LazaroDamasceno <lazarolawrence2@gmail.com>
2023-06-16 21:31:16 -0300

    commit

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		var graph = new MultiGraph();
-		graph.addVertex("A");
-		System.out.println(graph.getEdges());
+
+		Vertex v1 = new Vertex("A");
+		Vertex v2 = new Vertex("B");
+		Edge e1 = new Edge(100, v1, v2);
+		v1.add(e1);
+		v2.add(e1);
+		System.out.println(v1);
+
+		//MultiGraph mg = new MultiGraph();
+
+
 	}
 
 }
--- MultiGraph.java
+++ MultiGraph.java
@@ -1,19 +1,23 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultiGraph {
 	
 	private List<Vertex> vertices;
 	private List<Edge> edges;
-	
-	/*
-	 * 
-	 */
 
-	xxx
+	public MultiGraph() {
+		vertices = new ArrayList<>();
+		edges = new ArrayList<>();
+	}
 
-	/*
-	 * 
-	 */
+	public void addVertex(Vertex vertex) {
+		vertices.add(vertex);
+	}
+
+	public void addEdge(Edge edge) {
+		edges.add(edge);
+	}
 
 	public List<Vertex> getVertices() {
 		return vertices;
@@ -30,5 +34,21 @@ public class MultiGraph {
 	public void setEdges(List<Edge> edges) {
 		this.edges = edges;
 	}
-	
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (var obj : edges) {
+			sb.append("----------------");
+			sb.append("v1");
+			sb.append(obj.getFirstVertex());
+			sb.append(" ------ ");
+			sb.append(obj.getData().toString());
+			sb.append(" ------ ");
+			sb.append(obj.getSecondVertex());
+			sb.append("----------------\n");
+		}
+		return sb.toString();
+	}
+
 }
