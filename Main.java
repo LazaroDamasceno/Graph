4329f08b42385c382cefbea5d5746ed08f2765e3
lazarodamasceno <lazarolawrence2@gmail.com>
2023-06-19 22:52:31 -0300

    commit

--- Main.java
+++ Main.java
@@ -3,36 +3,36 @@ public class Main {
 	public static void main(String[] args) {
 		MultiGraph mg = new MultiGraph();
 		// Primeiras adições
-		var A = mg.addVertex("A");
-		var B = mg.addVertex("B");
-		var e1 = mg.addEdge(1, A, B);
-		A.add(e1);
-		B.add(e1);
+		var v1 = mg.addVertex("A");
+		var v2 = mg.addVertex("B");
+		var e1 = mg.addEdge(1, v1, v2);
+		v1.add(e1);
+		v2.add(e1);
 		// Segundas adições
-		var C = mg.addVertex("C");
-		var e2 = mg.addEdge(2, A, C);
-		A.add(e2);
-		C.add(e2);
+		var v3 = mg.addVertex("C");
+		var e2 = mg.addEdge(2, v1, v3);
+		v1.add(e2);
+		v3.add(e2);
 		// Terceiras adições
-		var D = mg.addVertex("D");
-		var e3 = mg.addEdge(3, B, D);
-		B.add(e3);
-		D.add(e3);
+		var v4 = mg.addVertex("D");
+		var e3 = mg.addEdge(3, v2, v4);
+		v2.add(e3);
+		v4.add(e3);
 		System.out.println(mg);
 		// Remove arresta
 		mg.removeEdge(e1);
 		System.out.println(mg);
 		// Remove vertice
-		mg.removeVertex(A);
+		mg.removeVertex(v1);
 		System.out.println(mg);
 		// Substitui vertice
-		mg.replaceDataVertix(B, "F");
+		mg.replaceDataVertix(v2, "F");
 		System.out.println(mg);
 		// Substitui arresta
 		mg.replaceDataEdge(e3, 33);
 		System.out.println(mg);
 		// Mostrando os grafos
-		mg.isAdjacent(null, null);
+		mg.isAdjacent(v2, v4);
 
  	}
 
